1f524b1d6e7d9cf3a338ee68c101406bfad05192
lizaveta9357 <91557414+lizaveta9357@users.noreply.github.com>
2022-02-09 20:54:30 -0500

    Added before hook for auth spec. (#5)
    Added root before hook

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -1,32 +1,50 @@
 import supertest from 'supertest';
 import {expect} from "chai";
-import 'dotenv/config'
 
 describe('Auth', function() {
     const request = supertest(process.env.BASE_URL) // Where did PROCESS come from?
-    // console.log(request);
 
-    it('Successful Log In', function() {
-        request
-            .post('/auth') // /auth is endpoint
-            .send({ login: process.env.LOGIN, password: process.env.PASSWORD }) // request body
-            //.expect(500) // this expect is from supertest?? and it doesn't work
-            .end(function(err, res) {  // works with response
-                // console.log(res);
-                expect(res.statusCode).to.eq(200) // one of the response object keys
-                expect(res.body.token).not.to.be.undefined // .body.token is method that calls token
-            })
+    describe('Successful Log In', function(){
+        let result;
+
+        //HOOK BEFORE
+        before(async function() {
+            await request
+                .post('/auth') // /auth is endpoint
+                .send({ login: process.env.LOGIN, password: process.env.PASSWORD }) // request body
+                .then(res => { // explanation please. What is .then??
+                    // What is it in the res? Response body? Ok. Who said that?
+                    result = res;
+                })
+        })
+
+        it('response status code is 200', function() {
+            expect(result.statusCode).to.eq(200) // one of the response object key
+        })
+
+        it('response body contains authorization token', function() {
+            expect(result.body.token).not.to.be.undefined
+        })
     })
 
-    it('Login with Invalid Credentials', function() {
-        request
-            .post('/auth')
-            .send({ login: 'adminius111', password: 'supers3cret' })
-            .end(function(err, res) {
-                // console.log(res);
-                expect(res.statusCode).to.eq(404)
-                expect(res.body.message).to.eq('Wrong login or password.')
-                // We need postman to know the message text??
-            })
+    describe('Login with Invalid Credentials', function() {
+        let result;
+
+        // HOOK BEFORE
+        before(async function() { // Do we use HOOK BEFORE only inside descride?? Yes
+            await request
+                .post('/auth')
+                .send({login: 'adminius111', password: 'supers3cret'})
+                .then(res => {
+                    result = res;
+                })
+        })
+
+        it('response status code is 404', function () {
+            expect(result.statusCode).to.eq(404);
+        })
+        it('response body contains error massage', function () {
+            expect(result.body.message).to.eq('Wrong login or password.')
+        })
     })
 })
