ec9a4ce24e3ebc1aa12111b3dfc7791e731ba0a6
lizaveta9357 <91557414+lizaveta9357@users.noreply.github.com>
2022-02-11 23:39:03 -0500

    Added auth helper (#6)

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -1,50 +1,38 @@
-import supertest from 'supertest';
-import {expect} from "chai";
+import {expect} from 'chai';
+import AuthHelper from '../helpers/auth.helper';
 
 describe('Auth', function() {
-    const request = supertest(process.env.BASE_URL) // Where did PROCESS come from?
+     // Where did PROCESS come from? process from package DOTENV
+
+    let authHelper = new AuthHelper();
 
     describe('Successful Log In', function(){
-        let result;
 
-        //HOOK BEFORE
         before(async function() {
-            await request
-                .post('/auth') // /auth is endpoint
-                .send({ login: process.env.LOGIN, password: process.env.PASSWORD }) // request body
-                .then(res => { // explanation please. What is .then??
-                    // What is it in the res? Response body? Ok. Who said that?
-                    result = res;
-                })
+            await authHelper.login(process.env.LOGIN, process.env.PASSWORD)
         })
 
         it('response status code is 200', function() {
-            expect(result.statusCode).to.eq(200) // one of the response object key
+            expect(authHelper.response.statusCode).to.eq(200) // one of the response object key
         })
 
         it('response body contains authorization token', function() {
-            expect(result.body.token).not.to.be.undefined
+            expect(authHelper.response.body.token).not.to.be.undefined
         })
     })
 
     describe('Login with Invalid Credentials', function() {
-        let result;
 
-        // HOOK BEFORE
         before(async function() { // Do we use HOOK BEFORE only inside descride?? Yes
-            await request
-                .post('/auth')
-                .send({login: 'adminius111', password: 'supers3cret'})
-                .then(res => {
-                    result = res;
-                })
+            await authHelper.login('invalid', 'invalid');
         })
 
         it('response status code is 404', function () {
-            expect(result.statusCode).to.eq(404);
+            expect(authHelper.response.statusCode).to.eq(404);
         })
+
         it('response body contains error massage', function () {
-            expect(result.body.message).to.eq('Wrong login or password.')
+            expect(authHelper.response.body.message).to.eq('Wrong login or password.')
         })
     })
 })
